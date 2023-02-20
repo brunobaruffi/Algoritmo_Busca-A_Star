f43762afcc0866252b0d42dfebeb3690742867ea
brunobaruffi <brunobaruffi@gmail.com>
2023-02-19 22:41:38 -0300

    feat: Criação do metodo 'verNovosVizinhos' cujo objetivo é listar um vetor com todos os vizinhos validos da coordenada passada

--- projeto.py
+++ projeto.py
@@ -1,44 +1,97 @@
 
 class buscar_labirinto:
     
-    def __init__(self, corpo, inicio, fim):
+    def __init__(self, corpo, inicio, fim, tamanho):
         self.corpo = corpo
         self.iniciox = inicio[0]
         self.inicioy = inicio[1]
         self.fimx = fim[0]
         self.fimy = fim[1]
+        self.tamanhoX = tamanho[0]  #linhas
+        self.tamanhoY = tamanho[1]  #colunas
         self.vetorFechado = []
         self.vetorAberto = []
-        
-
+                
+        #self.vetorAberto.append([3,2,1,12])
+        #self.vetorFechado.append([2,2,1,12])
 
 
 
     def verNovosVizinhos(self, x, y):
+        saida = []
+        #O algoritmo abaixo verifica todos os 8 vizinhos 
+
         #x+1,y-1
+        if ((y-1 >= 0) and (x+1 <= self.tamanhoX)):
+            if(self.corpo[x+1][y-1] == 1):               
+                if (self.verificarExistenciaVetorAberto(x+1, y-1) == False):
+                    if(self.verificarExistenciaVetorFechado(x+1, y-1) == False):
+                        saida.append([x+1,y-1])
+                        print("x+1,y-1")
+                      
         #x+1,y
+        if ((x+1 <= self.tamanhoX)):
+            if(self.corpo[x+1][y] == 1):
+                if (self.verificarExistenciaVetorAberto(x+1, y) == False):
+                    if(self.verificarExistenciaVetorFechado(x+1, y) == False):
+                        saida.append([x+1,y])
+                        print("x+1,y")
         #x+1,y+1
+        if ((y+1 <=self.tamanhoY) and (x+1 <= self.tamanhoX)):
+            if(self.corpo[x+1][y+1] == 1):
+                if (self.verificarExistenciaVetorAberto(x+1, y+1) == False):
+                    if(self.verificarExistenciaVetorFechado(x+1, y+1) == False):
+                        saida.append([x+1,y+1])
+                        print("x+1,y+1")
         
         #x,y+1
+        if ((y+1 <=self.tamanhoY)):
+            if(self.corpo[x][y+1] == 1):
+                if (self.verificarExistenciaVetorAberto(x, y+1) == False):
+                    if(self.verificarExistenciaVetorFechado(x, y+1) == False):
+                        saida.append([x,y+1])
+                        print("x,y+1")
         #x,y-1
+        if ((y-1 >=0)):
+            if(self.corpo[x][y-1] == 1):
+                if (self.verificarExistenciaVetorAberto(x, y-1) == False):
+                    if(self.verificarExistenciaVetorFechado(x, y-1) == False):
+                        saida.append([x,y-1])
+                        print("x,y-1")
         
         #x-1,y-1
+        if ((y-1 >= 0) and (x-1 >= 0)):
+            if(self.corpo[x-1][y-1] == 1):
+                if (self.verificarExistenciaVetorAberto(x-1, y-1) == False):
+                    if(self.verificarExistenciaVetorFechado(x-1, y-1) == False):
+                        saida.append([x-1,y-1])
+                        print("x-1,y-1")
         #x-1,y
+        if ((x-1 >= 0)):
+            if(self.corpo[x-1][y] == 1):
+                if (self.verificarExistenciaVetorAberto(x-1, y) == False):
+                    if(self.verificarExistenciaVetorFechado(x-1, y) == False):
+                        saida.append([x-1,y])
+                        print("x-1,y")
         #x-1,y+1
-        
-        #if(self.corpo[x+1,y])
-        #print (self.fimx)
-        #self.vetorAberto.append([3,2,1,12])
-        temp = self.verificarVetorAberto(x,y)
-        print (temp)
+        if ((y+1 <= self.tamanhoY) and (x-1 >= 0)):
+            if(self.corpo[x-1][y+1] == 1):
+                if (self.verificarExistenciaVetorAberto(x-1, y+1) == False):
+                    if(self.verificarExistenciaVetorFechado(x-1, y+1) == False):
+                        saida.append([x-1,y+1])
+                        print("x-1,y+1")
+                      
+        return saida
 
     def verificarExistenciaVetorAberto(self,x,y):
+        #Roda o vetor aberto conferindo se o x,y esta la
         for i in self.vetorAberto:
             if((i[0] == x) and (i[1] == y)):
                 return True
         return False
     
     def verificarExistenciaVetorFechado(self,x,y):
+        #Roda o vetor fechado conferindo se o x,y esta la
         for i in self.vetorFechado:
             if((i[0] == x) and (i[1] == y)):
                 return True
@@ -55,15 +108,17 @@ corpo = [  # 0,1,2,3,4,5,6,7
             [1,1,1,1,0,1,1,1], # 2
             [1,1,1,1,1,1,1,1]  # 3
         ]
+#Tamanho de x,y(linhas e colunas)
+tamanho = [3,7]
 #indica o ponto de partida do vetor
 inicio = [0,0]
 #indica o ponto final de chegada
-final = [7,0]
+final = [0,7]
 
 
-chamada = buscar_labirinto(corpo, inicio, final)
+chamada = buscar_labirinto(corpo, inicio, final, tamanho)
 
-chamada.verNovosVizinhos(3, 3)                        
+print(chamada.verNovosVizinhos(2, 3))
 
 
 
